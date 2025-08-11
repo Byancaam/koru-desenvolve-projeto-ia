4c41f7273c189655bc3e6e4b9840f28bf301624d
Bianca Amarante Magalhães <164433450+byancaaam@users.noreply.github.com>
2025-08-11 16:17:13 -0300

    feat: adicionar validação de inputs e estado de carregamento no botão

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-const apiKey = document.getElementById('apiKeyInput').value;
-const question = document.getElementById('questionInput').value;
-const answerElement = document.getElementById('answerText').value;
+const apiKeyInput = document.getElementById('apiKeyInput');
+const questionInput = document.getElementById('questionInput');
+const answerElement = document.getElementById('answerText');
+const askButton = document.getElementById('askButton');
+
+askButton.addEventListener('click', function () {
+  if (!apiKeyInput.value) {
+    alert('Por favor, insira sua Api key?');
+    return;
+  }
+
+  if (!questionInput.value) {
+    alert('Por favor, insira a sua pergunta!');
+    return;
+  }
+
+  // Loading
+  askButton.disabled = true;
+  askButton.textContent = 'Carregando ...';
+
+  setTimeout(function () {
+    askButton.disabled = false;
+    askButton.textContent = 'Perguntar';
+  }, 2000);
+
+  console.log('Pronto para fazer a requisição');
+});
